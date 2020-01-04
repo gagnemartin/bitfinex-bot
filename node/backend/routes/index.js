373c1c6795a5e79c635e63afb4d29c00a251a629
Martin Gagné <martin_gagne@outlook.com>
2020-01-04 02:40:32 -0500

    MACD and RSI

--- node/backend/routes/index.js
+++ node/backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios')
+const indicators = require('technicalindicators')
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -8,9 +9,14 @@ const axios = require('axios')
 // });
 
 router.get('/candles', function(req, res, next) {
+  const now = Date.now()
+  const period = 14
+  const startPeriod = now - (1000 * 60 * 60 * 24 * period)
   const domain = 'https://api-pub.bitfinex.com/v2/'
   const path = 'candles/trade:30m:tBTCUSD/hist'
-  const url = domain + path
+  const params = '?sort=1&limit=10000&start=' + startPeriod
+  const url = domain + path + params
+
 
   axios.get(url)
     .then(response => {
@@ -25,7 +31,31 @@ router.get('/candles', function(req, res, next) {
           }
         })
 
-        res.send(candles);
+        const closes = candles.map(candle => candle.close)
+        const { MACD, RSI } = indicators
+        const macdInput = {
+          values            : closes,
+          fastPeriod        : 12,
+          slowPeriod        : 26,
+          signalPeriod      : 9,
+          SimpleMAOscillator: false,
+          SimpleMASignal    : false
+        }
+
+        const macd = MACD.calculate(macdInput)
+
+        const rsiInput = {
+          values: closes,
+          period: period
+        }
+
+        const rsi = RSI.calculate(rsiInput)
+
+        const data = {
+          candles, macd, rsi
+        }
+
+        res.send(data);
         //res.send(data.data);
       } else {
         res.status(500).send('Error')
