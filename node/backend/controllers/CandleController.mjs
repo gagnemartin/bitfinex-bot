b8a2df7432306348581eba0bb9b7ba2f9fd3277a
Martin Gagné <martingagne90@gmail.com>
2020-01-17 00:59:27 -0500

    refactor, change 30m to 5m  and stop loss management

--- node/backend/controllers/CandleController.mjs
+++ node/backend/controllers/CandleController.mjs
@@ -5,6 +5,19 @@ import AppController from './AppController.mjs'
 class CandleController extends AppController {
   constructor(model) {
     super(model)
+
+    this.candles = []
+    this.closes = []
+    this.lastTrade = { position: 'buy', close: 7219.3 }
+    this.thresholdIncrease = 2.5
+    this.thresholdDecrease = 2.5
+
+    //this.startBtc = 1
+    this.startBtc = 0.01315
+    //this.startUsd = 7219.3
+    this.startUsd = 0.19332
+    this.btc = this.startBtc
+    this.usd = this.startUsd
   }
 
   tickers = (req, res, next) => {
@@ -54,17 +67,17 @@ class CandleController extends AppController {
 
   fetch = (req, res, next) => {
     const now = Date.now()
-    const period = 134
+    const period = 7
     const startPeriod = now - (1000 * 60 * 60 * 24 * period)
     const domain = 'https://api-pub.bitfinex.com/v2/'
-    const path = 'candles/trade:30m:tBTCUSD/hist'
+    const path = 'candles/trade:5m:tBTCUSD/hist'
     const params = '?sort=1&limit=10000&start=' + startPeriod
     const url = domain + path + params
 
     axios.get(url)
       .then(response => {
         if (typeof response.data !== 'undefined') {
-          const candles = response.data.map(candle => {
+          this.candles = response.data.map(candle => {
             return {
               date: new Date(candle[0]),
               open: candle[1],
@@ -74,150 +87,31 @@ class CandleController extends AppController {
             }
           })
 
-          const closes = candles.map(candle => candle.close)
-          const { MACD, RSI, SMA, bullish, bearish } = indicators
-          const macdInput = {
-            values            : closes,
-            fastPeriod        : 12,
-            slowPeriod        : 26,
-            signalPeriod      : 9,
-            SimpleMAOscillator: false,
-            SimpleMASignal    : false
-          }
+          this.closes = this.candles.map(candle => candle.close)
 
-          const sma = SMA.calculate({ period: 8, values: closes })
-          const smaDifference = candles.length - sma.length
+          this.mergeSma()
+          this.mergeMacd()
+          this.mergeRsi()
+          this.mergeBullishBearish()
+          this.calculatePositions()
 
-          const macd = MACD.calculate(macdInput)
-          const macdDifference = candles.length - macd.length
-
-          const rsiInput = {
-            values: closes,
-            period: 14
-          }
-
-          const rsi = RSI.calculate(rsiInput)
-          const rsiDifference = candles.length - rsi.length
-
-          let macdIndex = 0
-          let rsiIndex = 0
-          let smaIndex = 0
-          //let startBtc = 1
-          let startBtc = 0.01315
-          let startUsd = 0.19332
-          let btc = startBtc
-          let usd = startUsd
-          let lastTrade = 'BUY'
-
-          candles.map((candle, i) => {
-            if (typeof sma[smaIndex] !== 'undefined' && i >= smaDifference) {
-              candles[i].sma = sma[smaIndex]
-
-              // if (smaIndex > 0) {
-              //   const previous = sma[smaIndex - 1]
-              //   const current = sma[smaIndex]
-              //
-              //   candles[i].trend = current > previous ? 'up' : 'down'
-
-                //if (smaIndex > 2) {
-                  const trends = candles.slice(i - 3, i)
-
-                  if (trends.length > 0) {
-                    candles[i].bullish = bullish({
-                      open: trends.map(trend => trend.open),
-                      close: trends.map(trend => trend.close),
-                      high: trends.map(trend => trend.high),
-                      low: trends.map(trend => trend.low),
-                    })
-
-                    candles[i].bearish = bearish({
-                      open: trends.map(trend => trend.open),
-                      close: trends.map(trend => trend.close),
-                      high: trends.map(trend => trend.high),
-                      low: trends.map(trend => trend.low),
-                    })
-
-                    if (candles[i].bullish !== candles[i].bearish) {
-                      if (lastTrade === 'SELL' && candles[i].bullish) {
-                        candles[i].trade = 'BUY'
-                      } else if (lastTrade === 'BUY' && candles[i].bearish) {
-                        candles[i].trade = 'SELL'
-                      }
-                    }
-                  }
-                //}
-              //   }
-              // }
-              smaIndex++
-            }
-
-            if (typeof macd[macdIndex] !== 'undefined' && i >= macdDifference) {
-              candles[i].macd = macd[macdIndex]
-
-              if (macdIndex > 0) {
-                const current = macd[macdIndex]
-                const previous = macd[macdIndex -1]
-
-                if (lastTrade === ' BUY' && previous.MACD > previous.signal && current.MACD < current.signal && candles[i].bearish) {
-                //if (current.histogram <= 5 && candles[i].bearish) {
-                  candles[i].trade = 'SELL'
-                } else if (lastTrade === 'SELL' && previous.MACD < previous.signal && current.MACD > current.signal && candles[i].bullish) {
-                //} else if (current.histogram >= -5 && candles[i].bullish) {
-                  candles[i].trade = 'BUY'
-                }
-
-              }
-              macdIndex++
-            }
-
-            if (typeof rsi[rsiIndex] !== 'undefined' && i >= rsiDifference) {
-              candles[i].rsi = rsi[rsiIndex]
-              // const previous = rsi[rsiIndex -1]
-
-              if (typeof candles[i].trade !== 'undefined') {
-                if (rsi[rsiIndex] > 70 && lastTrade === 'BUY' && candles[i].bearish) {
-                    candles[i].trade = 'SELL'
-                } else if (rsi[rsiIndex] < 30 && lastTrade === 'SELL' && candles[i].bullish) {
-                  candles[i].trade = 'BUY'
-                }
-              }
-              rsiIndex++
-            }
-
-            if (typeof candles[i].trade !== 'undefined') {
-              if (lastTrade === 'BUY' && candles[i].trade === 'SELL') {
-                lastTrade = 'SELL'
-                usd = candle.close * btc
-                btc = 0
-
-                candles[i].wallet = { btc, usd }
-              } else if (lastTrade === 'SELL' && candles[i].trade === 'BUY') {
-                  lastTrade = 'BUY'
-                  btc = usd / candle.close
-                  usd = 0
-
-                  candles[i].wallet = { btc, usd }
-              }
-            }
-          })
-
-          const trades = candles.filter(candle => typeof candle.trade !== 'undefined')
-
-          const data = {
-            trades: trades.length,
+          res.send({
             start: {
-              btc: startBtc + ' (' + startBtc * candles[0].close + '$)',
-              usd: startUsd
+              btc: this.startBtc + ' (' + this.startBtc * this.candles[0].close + '$)',
+              usd: this.startUsd
             },
             end: {
-              btc: btc + ' (' + btc * candles[candles.length -1].close + '$)',
-              usd: usd + ' (' + usd / candles[candles.length -1].close + '$)'
+              btc: this.btc + ' (' + this.btc * this.candles[this.candles.length -1].close + '$)',
+              usd: this.usd + ' (' + this.usd / this.candles[this.candles.length -1].close + '$)'
             },
-            candles: trades//, macd, rsi
-          }
-
-          res.send(data);
-          //res.send(data.data);
+            numbers: {
+              candles: this.candles.length,
+              trades: this.candles.filter(candle => candle.position !== null).length,
+              closes: this.closes.length,
+            },
+            trades: this.candles.filter(candle => candle.position !== null),
+            //candles: this.candles
+          })
         } else {
           res.status(500).send('Error')
         }
@@ -226,6 +120,225 @@ class CandleController extends AppController {
         res.status(e.response.status).send(e)
         console.error(e.response.status)
       })
+  }
+
+  /**
+   * Add the SMA for a given candle object
+   */
+  mergeSma = () => {
+    const { SMA } = indicators
+    const input = {
+      values: this.closes,
+      period: 8
+    }
+    const sma = SMA.calculate(input)
+    const difference = this.candles.length - sma.length
+
+    let iSma = 0
+    for (let i = 0; i < this.candles.length; i++) {
+      const candle = this.candles[i]
+
+      if (i >= difference) {
+        candle.sma = sma[iSma]
+
+        iSma++
+      } else {
+        candle.sma = null
+      }
+    }
+  }
+
+  mergeMacd = () => {
+    //const { MACD, RSI, SMA, bullish, bearish } = indicators
+    const { MACD } = indicators
+    const input = {
+      values            : this.closes,
+      fastPeriod        : 12,
+      slowPeriod        : 26,
+      signalPeriod      : 9,
+      SimpleMAOscillator: false,
+      SimpleMASignal    : false
+    }
+
+    const macd = MACD.calculate(input)
+    const difference = this.candles.length - macd.length
+
+    let iSma = 0
+    for (let i = 0; i < this.candles.length; i++) {
+      const candle = this.candles[i]
+
+      if (i >= difference) {
+        candle.macd = macd[iSma]
+
+        iSma++
+      } else {
+        candle.macd = null
+      }
+    }
+  }
+
+  mergeBullishBearish = () => {
+    const { bullish, bearish } = indicators
+
+    for (let i = 0; i < this.candles.length; i++) {
+      const candle = this.candles[i]
+
+      candle.bullish = null
+      candle.bearish = null
+
+      if (i >= 7) {
+        const trends = this.candles.slice(i - 7, i)
+
+        candle.bullish = bullish({
+          open: trends.map(trend => trend.open),
+          close: trends.map(trend => trend.close),
+          high: trends.map(trend => trend.high),
+          low: trends.map(trend => trend.low),
+        })
+
+        candle.bearish = bearish({
+          open: trends.map(trend => trend.open),
+          close: trends.map(trend => trend.close),
+          high: trends.map(trend => trend.high),
+          low: trends.map(trend => trend.low),
+        })
+      } else {
+
+      }
+    }
+  }
+
+  mergeRsi = () => {
+    const { RSI } = indicators
+    const input = {
+      values: this.closes,
+      period: 14
+    }
+
+    const rsi = RSI.calculate(input)
+    const difference = this.candles.length - rsi.length
+
+    let iRsi = 0
+    for (let i = 0; i < this.candles.length; i++) {
+      const candle = this.candles[i]
+
+      if (i >= difference) {
+        candle.rsi = rsi[iRsi]
+
+        iRsi++
+      } else {
+        candle.rsi = null
+      }
+    }
+  }
+
+  calculatePositions = () => {
+    for (let i = 0; i < this.candles.length; i++) {
+      const candle = this.candles[i]
+      const lastTrade = this.lastTrade
+
+      candle.position = null
+
+      if (this.hasEnoughData(candle)) {
+        if (
+          this.isBullish(candle) &&
+          this.brokeSupport(candle) &&
+          (typeof lastTrade.position === 'undefined' || lastTrade.position === 'sell') &&
+          this.hasLostEnough(candle)
+        ) {
+        //if (this.isBullish(candle) && candle.rsi <= 30 && (typeof lastTrade.position === 'undefined' || lastTrade.position === 'sell')) {
+          candle.position = 'buy'
+          console.log('BUY: ', this.thresholdDecrease, this.percentageDecrease(candle))
+          this.lastTrade = candle
+
+          this.btc = this.usd / candle.close
+          this.usd = 0
+        } else if (
+          this.isBearish(candle) &&
+          this.brokeResistance(candle) &&
+          (typeof lastTrade.position === 'undefined' || lastTrade.position === 'buy') &&
+          this.hasGainedEnough(candle)
+        ) {
+        //} else if (this.isBearish(candle) && candle.rsi >= 70 && (typeof lastTrade.position === 'undefined' || lastTrade.position === 'buy')) {
+          candle.position = 'sell'
+          console.log('SELL: ', this.thresholdIncrease, this.percentageIncrease(candle))
+          this.lastTrade = candle
+
+          this.usd = candle.close * this.btc
+          this.btc = 0
+        }
+      }
+    }
+  }
+
+  hasEnoughData = data => {
+    return (data.macd !== null && typeof data.macd.histogram !== 'undefined') && data.rsi !== null && data.bearish !== null && data.bullish !== null
+  }
+
+  hasGainedEnough = data => {
+    const percentage = this.percentageIncrease(data)
+
+    if (percentage <= -3 && this.hoursBetween(this.lastTrade.date, data.date) >= 1) {
+      this.thresholdIncrease = 2.5
+      return true
+    }
+    const isWithinThreshold = percentage >= this.thresholdIncrease
+
+    if (isWithinThreshold) {
+      this.thresholdIncrease = 2.5
+    } else {
+      this.thresholdIncrease -= 0.01
+    }
+
+    return isWithinThreshold
+  }
+
+  hasLostEnough = data => {
+    const percentage = this.percentageDecrease(data)
+
+    if (percentage <= -1 && this.hoursBetween(this.lastTrade.date, data.date) >= 1) {
+      this.thresholdDecrease = 2.5
+      return true
+    }
+
+    const isWithinThreshold = percentage >= this.thresholdDecrease
+
+    if (isWithinThreshold) {
+      this.thresholdDecrease = 2.5
+    } else {
+      this.thresholdDecrease -= 0.01
+    }
+    console.log(this.hoursBetween(this.lastTrade.date, data.date))
+
+    return isWithinThreshold
+  }
+
+  hoursBetween = (d1, d2) => {
+    return ((((Date.parse(d2) - Date.parse(d1)) / 1000) / 60) / 60)
+  }
+
+  percentageIncrease = data => {
+    return ((data.close - this.lastTrade.close) / this.lastTrade.close) * 100
+  }
+
+  percentageDecrease = data => {
+    return ((this.lastTrade.close - data.close) / this.lastTrade.close) * 100
+  }
+
+  brokeResistance = data => {
+    return data.sma <= data.low
+  }
+
+  brokeSupport = data => {
+    return data.sma >= data.high
+  }
+
+  isBullish = data => {
+    return data.bullish && !data.bearish
+  }
+
+  isBearish = data => {
+    return data.bearish && !data.bullish
   }
 }
 
